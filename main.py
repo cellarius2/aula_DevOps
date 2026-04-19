d14a70cbd5bc5005fd366731980b391cc36743f2
Cellarius <emilydomingues014@gmail.com>
2026-04-19 14:08:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ async def root():
     return {"message": "Hello World"}
 
 #http://127.0.0.1:8000/teste1
-@app.get("/teste1")
+@app.get("/teste")
 async def test():
     return {"Status:": "Completo"}
